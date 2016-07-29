8c6a8faf8a74fb9fba9997680cbee078dd392cd0
Margierain <margaret.ochieng@andela.com>
2016-07-29 12:50:02 +0300

    Delete .heroku-config.sh

--- .heroku-config.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-heroku config:set MAIL_PASSWORD=nwmmdergfeqepfqw
-heroku config:set MAIL_USERNAME=margaretrain.mo@gmail.com
-heroku config:set SECRET_KEY=JdP7686iWVNnQclaOGxzMR8PA2iAdANP8O2uPF2RzBCeHTv3eI
-heroku config:set JOKENIA_ADMIN=margaretrain.mo@gmail.com
-heroku config:set JOKEIA_CONFIG=production
